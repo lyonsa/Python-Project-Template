73a9e6a4a7fd1900f514bfc048c0428b8974a89e
Allen Lyons <allyons16@gmail.com>
2025-03-25 14:56:50 -0400

    remove more files

--- .devcontainer/postStartBackground.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-poetry run ptyme-track --standalone
--- .devcontainer/postStartCommand.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-# run in the background at startup
-nohup bash -c ".devcontainer/postStartBackground.sh &" > .dev_container_logs/postStartBackground.out
